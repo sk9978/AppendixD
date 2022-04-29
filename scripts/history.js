75d51c61f86f0b212257d79f15a1244e9695ca7e
sk9978 <104648960+sk9978@users.noreply.github.com>
2022-04-29 14:17:23 -0400

    Add files via upload

---
+++ scripts/history.js
@@ -0,0 +1,144 @@
+$("#history_back").on("click",function()
+{
+	slideright("list_of_history","profile_screen");
+	
+	setTimeout(function()
+	{
+		$("#list_of_history_screen").empty();
+	},500);
+});
+
+//history.js
+
+var myDatabase;
+
+$(document).ready(function(){
+	$(".back").css({
+    	"position": "absolute",
+      	"z-index": "999"
+    });
+  
+  	var height = $(document).height();
+  
+  	$("#health_history_ul").css({
+    	"top": "45px",
+      	"height": height-45
+    });
+  
+  	myDatabase - new myDB();
+  	myDatabase.appendDivId = "health_history_ul";
+  	myDatabase.openDB();
+  	myDatabase.getHistory();
+});
+
+$("#history_back").on("click", function(){
+	sliderright("list_of_history_screen", "profile_screen");
+  	setTimeout(function(){
+    	$("#list_of_history_screen").empty();
+    }, 500);
+});
+
+$("#add_new_item").on("click", function(){
+	$("#add_item_screen").load("add_item.html", function(){
+    	
+    });
+  	$("#add_item_screen").css({
+    	"z-index": "999"
+    });
+  	slideoneup("add_item_screen");
+});
+
+var myDB = function(){
+	this.db;
+  	this.appendDivId;
+  	
+  	this.openDB = function(){
+    	this.db = openDatabase("History", "1.0", "Items", 2*1024*1024);
+     	this.db.transaction(function(tx){
+        	tx.executeSql("CREATE TABLE IF NOT EXISTS history (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, now, create, title, notes)");
+        });
+    };
+  
+  	this.getHistory = function(){
+    	var self = this;
+      
+      	this.db.transaction(function(tx){
+        	tx.executeSql("SELECT * FROM history", [], querySuccess, errorCB);
+        });
+      
+      	function querySuccess(tx, results){
+        	var len = results.rows.length;
+          	if(len > 0){
+            	var htmlArray = [];
+              	for (var i=0; i<len; i++){
+                	var html = "<li id='" + results.rows.item(i).now + "' class='history_li'><span class='delete_item'>x</span><span class='h_title'>" + results.rows.item(i).title + "</span><span class='h_notes'>" + results.rows.item(i).notes + "</span></li>";
+                  	$("#"+self.appendDivId).append(html);
+                  	var id = results.rows.item(i).now;
+                  	$("#" + results.rows.item(i).now).children(".delete_item").on("click", function(){
+                    	self.deleteQuery(id);
+                    });
+                }
+            }
+          	else {
+            	
+            }
+        }
+      
+      	function errorCB(err){
+        	alert("Well, We're Embarressed");
+        }
+    };
+  
+  	this.enterNewItem = function(obj){
+    	var self = this;
+      
+      	function populateDB(tx){
+        	tx.executeSql("INSERT INTO history(now, created, title, notes) VALUES(?, ?, ?, ?)", [obj.now, obj.now, obj.title, obj.notes]);
+        }
+      
+      	function errorCB(err){
+        	alert("There was an error");
+        }
+      
+      	function successCB(){
+        	$("#"+self.appendDivId).append("<li id='" + obj.now + "' class='history_li'><span class='delete_item'>x</span><span class='h_title'>" + obj.title + "</span><span class='h_notes'>" + obj.notes + "</span></li>");
+          	$("#"+obj.now).children(".delete_item").on("click", function(){
+            	self.deleteQuery(obj.now);
+            });
+        }
+      
+      	this.db.transaction(populateDB, errorCB, successCB);
+    };
+  
+  	this.deleteQuery = function(id){
+    	function deleteDB(tx){
+        	tx.executeSql("DELETE FROM history WHERE now = " + id + "");
+        }
+      
+      	function errorCB(err){
+        	alert("Whoops, That Was Strange!");
+        }
+      
+      	function successCB(){
+        	$("W"+id).remove();
+        }
+      
+      	this.db.transaction(deleteDB, errorCB, successCB);
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
---
+++ scripts/scripts.js
@@ -0,0 +1,48 @@
+
+
+	function helloWorld()
+	{
+		alert("Hello world");
+	}
+	
+$(document).ready(function()
+{});
+
+$("#health_history_button").on("click",function()
+{
+	slideleft("profile_screen","list_of_history_screen");
+	
+	$("#list_of_history_screen").load("history.html",function(){});
+});
+
+$("#settings_page_button").on("click",function()
+{
+	slideoneup("settings_screen");
+	
+	$("#settings_screen").load("settings.html",function(){});
+});
+
+$(document).ready(function()
+{
+	setProfileInfo();
+});
+
+function setProfileInfo()
+{
+	var localSettings=["name","age","weight","age"];
+	var ids=["my_name","date_of_birth","current_weight","current_age"];
+	
+	for (i=0;i<localSettings.length;i++)
+	{
+		var exists=localStorage.getItem(localSettings[i]);
+		if (exists)
+		{
+			$("#"+ids[i]).text(exists);
+		}
+		else
+		{
+		}
+	}
+	var dob=localStorage.getItem("age");
+	$("#current_age").text(dob);
+}
